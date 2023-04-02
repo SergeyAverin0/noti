a9dbe3872ea92b264651293b564c4e436355f1a0
SergeyAverin <pretty.street.003@gmail.com>
2023-04-02 14:18:26 +0300

    feat: added connection to mogodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
+import mongoose, { ConnectOptions } from 'mongoose';
 
 import routes from './routes/index';
 
@@ -7,10 +8,28 @@ const app: Express = express();
 const port = process.env.BACKEND_PORT;
 const host = process.env.BACKEND_HOST;
 
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_port = process.env.MONGODB_PORT;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_db_name = process.env.MONGODB_DB_NAME;
+
 
 app.use('/api/v1.0', routes);
 
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb://${mongodb_user}:${mongodb_password}@${mongodb_host}:${mongodb_port}/${mongodb_db_name}?authSource=admin`,
+      {  useNewUrlParser: true,  useUnifiedTopology: true } as ConnectOptions
+    );
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://${host}:${port}`);
+    });    
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://${host}:${port}`);
-});
+start();
