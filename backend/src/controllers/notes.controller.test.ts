cfef77ed7441096f31089a651fdd326444b24346
SergeyAverin <pretty.street.003@gmail.com>
2023-04-02 20:17:08 +0300

    test: changed first test

--- backend/src/controllers/notes.controller.test.ts
+++ backend/src/controllers/notes.controller.test.ts
@@ -1,11 +1,14 @@
-import request from 'supertest'
+import request from 'supertest';
 import { describe, expect, it } from '@jest/globals'
 
 import app from '../app'
 
-describe('GET /api/v1.0/notes', () => {
-  it('should return 200 status', async () => {
-    const res = await request(app).get('/api/v1.0/notes')
-    expect(res.statusCode).toBe(200)
-  })
-})
+
+describe("GET /api/v1.0/notes", () => {
+  it("should return 200 status", async () => {
+    const res = await request(app).get(
+    "/api/v1.0/notes"
+    );
+    expect(res.statusCode).toBe(200);
+  });
+});
