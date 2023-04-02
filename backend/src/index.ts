acc9755289a422fc073f15e1ca6914f67dfc1cb3
SergeyAverin <pretty.street.003@gmail.com>
2023-04-02 20:03:58 +0300

    test: added first test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,6 @@
-import express, { Express, ErrorRequestHandler } from 'express'
-import log4js from 'log4js'
-
-import routes from './routes/index'
+import app from './app'
 import connectToMongoDB from './models/connectToMongoDB'
 import notiConfig from './conf/noti.config'
-import log4jsConfig from './conf/log4js.config'
-
-const app: Express = express()
-
-// Create logger
-log4js.configure(log4jsConfig)
-const logger = log4js.getLogger()
-
-// Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(
-  log4js.connectLogger(log4js.getLogger('http'), {
-    level: 'auto',
-  }),
-)
-
-// Register router
-app.use('/api/v1.0', routes)
-
-// Error handler
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  logger.error(err.stack)
-  res.status(500).send('Something broke!')
-}
-
-app.use(errorHandler)
 
 // Start server
 const start = async () => {
