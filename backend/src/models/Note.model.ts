2ca9f6f41302facfd76fae2eeb54a1dab35f2f96
SergeyAverin <pretty.street.003@gmail.com>
2023-04-04 18:14:38 +0300

    feat: added note model
    This mongoose model for note. This model have information abaut note and note's cells.

---
+++ backend/src/models/Note.model.ts
@@ -0,0 +1,81 @@
+import { randomUUID } from 'crypto'
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+enum CellsType {
+  TEXT = 'text',
+  CAPTION = 'caption',
+  LINK = 'link',
+  LINE = 'line',
+  CHECK_BOX = 'checkBox',
+  CITATION = 'citation',
+  LIST = 'list',
+}
+
+interface ICells {
+  cellsType: CellsType
+  props: object
+  text: string
+}
+
+interface INote {
+  title: string
+  slug: string
+  isTrash: boolean
+  isBookmark: boolean
+  parentNote: INote
+  childNote: [INote]
+  cells: [ICells]
+}
+
+const CellsScheme = new mongoose.Schema<ICells>({
+  cellsType: {
+    type: String,
+    enum: Object.values(CellsType),
+    required: true,
+  },
+  props: mongoose.Schema.Types.Mixed,
+  text: String,
+})
+
+const NoteScheme = new Schema<INote>({
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50,
+  },
+  slug: {
+    type: String,
+  },
+  isTrash: {
+    type: Boolean,
+    default: false,
+  },
+  isBookmark: {
+    type: Boolean,
+    default: false,
+  },
+  parentNote: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Note',
+  },
+  childNote: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note',
+    },
+  ],
+  cells: [CellsScheme],
+})
+
+// Generated random slug for note
+NoteScheme.pre<INote>('save', function (next) {
+  if (!this.slug) {
+    this.slug = randomUUID()
+  }
+  next()
+})
+
+export const Note = mongoose.model<INote>('Note', NoteScheme)
