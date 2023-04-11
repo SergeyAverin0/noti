3cc70073393ef4d701feb9971a81152ccdf80002
SergeyAverin <pretty.street.003@gmail.com>
2023-04-11 16:06:02 +0300

    feat(NotesController): added create note feature in notes controller

--- backend/src/controllers/notes.controller.ts
+++ backend/src/controllers/notes.controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express'
 
+import NotesService from '../services/notes.service'
+
+
 class NotesController {
-  notesList(req: Request, res: Response) {
-    res.status(200).send('note list')
+  async get(req: Request, res: Response) {
+    const notes = await NotesService.getNoteList();
+    console.log(notes)
+    res.status(200).json(notes);
+  }
+  async post(req: Request, res: Response) {
+    const newNotes = await NotesService.createNote('Untitled');
+    res.status(201).json(newNotes);
   }
 }
 
