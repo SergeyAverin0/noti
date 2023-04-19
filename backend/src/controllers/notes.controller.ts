cffd7d5ea92ae7bb81d4e5f23d78d4927d00f4f6
SergeyAverin <pretty.street.003@gmail.com>
2023-04-19 16:44:14 +0300

    feat: added logger in controllers

--- backend/src/controllers/notes.controller.ts
+++ backend/src/controllers/notes.controller.ts
@@ -6,7 +6,6 @@ import NotesService from '../services/notes.service'
 class NotesController {
   async get(req: Request, res: Response) {
     const notes = await NotesService.getNoteList();
-    console.log(notes)
     res.status(200).json(notes);
   }
   async post(req: Request, res: Response) {
--- backend/src/controllers/singleNote.controller.ts
+++ backend/src/controllers/singleNote.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express'
+import log4js from 'log4js'
 
 import NotesService from '../services/notes.service'
 
+
+const logger = log4js.getLogger()
 
 interface ISingleNotesRequestParams {
     slug: string
@@ -10,18 +13,21 @@ interface ISingleNotesRequestParams {
 class SingleNotesController {
   async get(req: Request, res: Response) {
     const { slug } = req.params as unknown as ISingleNotesRequestParams;
-    const note = await NotesService.getNote(slug);
+    const note = await NotesService.getNote(slug);    
+    logger.info('get note ' + slug);
     res.status(200).json(note);
   }
   async patch(req: Request, res: Response) {
     const { slug } = req.params as unknown as ISingleNotesRequestParams;
     const updatedNoteBody = req.body
     const updatedNote = await NotesService.updateNote(slug, updatedNoteBody);
+    logger.info('updated note ' + slug);
     res.status(200).json(updatedNote);
   }
   async delete(req: Request, res: Response) {
     const { slug } = req.params as unknown as ISingleNotesRequestParams;
     await NotesService.deleteNote(slug);
+    logger.info('deleted note ' + slug);
     res.status(204).end();
   }
 }
